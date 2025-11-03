8d897c3fd8c397bfd0ade1dbcdac550517fffc0b
Jkamell <javierkamellyaspe@yahoo.com>
2025-11-03 17:07:51 -0500

    Aplicado Prettier

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import _ from 'lodash';
+import _ from "lodash";
 
-console.log(_.last(['uno', 'dos']));
+console.log(_.last(["uno", "dos"]));
 
 // Este mensaje aparecerá en la terminal cuando ejecutemos el proyecto
-console.log("¡Hola, Códica!");
+console.log("¡Hola, Códica!");  
